d96e193178b463f255ed2e0822a88035ac3d5221
jadelien1618 <jl9653@nyu.edu>
2021-06-22 19:31:57 +0800

    210622

---
+++ m/sub/assets/frame.js
@@ -0,0 +1,8 @@
+﻿
+document.write("<div id='bufferLeft'></div>"); 
+document.write("<div id='bufferRight'></div>");
+document.write("<div id='bufferBottom'></div>");
+document.write("<div id='bufferTop'></div>");
+document.write("<div id = 'line'></div>");
+
+		
