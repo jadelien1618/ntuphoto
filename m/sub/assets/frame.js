e55ebb34c05024adb1875e552887899cc5a46bcc
jadelien1618 <jl9653@nyu.edu>
2021-06-27 23:17:17 +0800

    update

--- m/sub/assets/frame.js
+++
@@ -1,8 +0,0 @@
-﻿
-document.write("<div id='bufferLeft'></div>"); 
-document.write("<div id='bufferRight'></div>");
-document.write("<div id='bufferBottom'></div>");
-document.write("<div id='bufferTop'></div>");
-document.write("<div id = 'line'></div>");
-
-		
--- sub/assets/frame.js
+++ sub/assets/frame.js
@@ -3,6 +3,7 @@ document.write("<div id='bufferLeft'></div>");
 document.write("<div id='bufferRight'></div>");
 document.write("<div id='bufferBottom'></div>");
 document.write("<div id='bufferTop'></div>");
-document.write("<div id = 'line'></div>");
+document.write("<div id='line' ></div>");
+
 
 		
