c6230c5d99d957ce6e60602f6849879c3301b950
jadelien1618 <jl9653@nyu.edu>
2021-06-29 11:37:17 +0800

    update

--- sub/assets/header.js
+++ sub/assets/header.js
@@ -2,7 +2,7 @@
 		
 document.write("<div class = 'headerOutercontainer'>");
 document.write("<div class = 'headercontainer'>");
-document.write("<a class = 'header' href = 'https://www.ntuphoto.tw/' target='_blank'>台<br>大<br>攝<br>影<br>社<br></a>");
+document.write("<a class='header' href = 'https://www.ntuphoto.tw/' target='_blank'>台<br>大<br>攝<br>影<br>社<br></a>");
 document.write("</div>");
 
 document.write("</div>");
@@ -10,13 +10,13 @@ document.write("</div>");
 document.write("<div class = 'headerOutercontainer2'>");
 
 document.write("<div class = 'headercontainer2'>");
-document.write("<a class = 'sideheader' href='nav.html' ><div id = 'rotate'>109-2</div>");
+document.write("<a class='sideheader' href='nav.html' ><div id = 'rotate'>109-2</div>");
 document.write("</div>");
 document.write("</div>");
 		
-document.write("<div class = 'headerOutercontainer3'>");
+document.write("<div class='headerOutercontainer3'>");
 
-document.write("<div class = 'headercontainer3'>");
-document.write("<a class = 'sideheader' href='comingsoon.html'><div id = 'rotate'>110-1</div></a>");
+document.write("<div class='headercontainer3'>");
+document.write("<a class='sideheader' href='comingsoon.html'><div id = 'rotate'>110-1</div></a>");
 document.write("</div>");
 document.write("</div>");
